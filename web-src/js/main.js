8a5a8ac1d15bf9181c3c23431cb5fc1a0e26c2f5
Roma48 <myzfann@mail.ru>
2015-12-13 15:50:35 +0200

    create infinite scroll

--- web-src/js/main.js
+++ web-src/js/main.js
@@ -1,1 +1,40 @@
+(function($){
+    var loading = false;
+    var last_page = false;
+    var page = 2;
 
+    function loadItems() {
+        loading = true;
+        $.ajax({
+            type: "GET",
+            url: "/page?page=" + page,
+            success: function(data) {
+                if(data.length > 0){
+                    $('tbody').append(data);
+                } else {
+                    last_page = true;
+                }
+            },
+            error: function(data) {
+                alert(data)
+            }
+        });
+        loading = false;
+        page++;
+    }
+
+    $(window).scroll(function() {
+        var scrollTop = $(window).scrollTop(),
+            docHeight = $(document).height(),
+            winHeight = $(window).height(),
+            scrollTrigger = 0.80;
+
+        if ((scrollTop / (docHeight - winHeight)) > scrollTrigger) {
+
+            if (last_page === false && loading === false) {
+                loadItems();
+            }
+        }
+    });
+
+}(jQuery));
